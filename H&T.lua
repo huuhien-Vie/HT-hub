0f0413a16c0007603087e67ddaea6f061cd2fb68
huuhien-Vie <phamthituqni76@gmail.com>
2026-03-16 21:47:33 +0700

    Create H&T.lua

---
+++ H&T.lua
@@ -0,0 +1,7 @@
+-- this script created at https://lambloxfruitscript.vercel.app/
+getgenv().ScriptTitle = "H&T hub"
+getgenv().ScriptSubTitle = "V1.0.0"
+getgenv().ScriptImage = "https://i.ibb.co/XH9zZbG/1000093092.png"
+getgenv().ScriptAuthorName = "Hữu Hiền k12"
+getgenv().ScriptAuthorSubTitle = "TikTok: bapcaisuper2"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/hoannhatz/Script/refs/heads/main/EZScript.lua"))()
